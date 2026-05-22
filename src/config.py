e8d5f16a882c9eb2661a1a6f9f708ad006ea73d0
Yekta Cengiz <yekta.cengiz@kocaeli.edu.tr>
2026-05-22 13:40:00 +0300

    Pencere boyutu, alfabe boyutu ve veri bolme parametrelerinin config dosyasina eklenmesi

--- src/config.py
+++ src/config.py
@@ -4,3 +4,14 @@ ANA_DIZIN = r"D:\yazlab2"
 VERI_DIZINI = os.path.join(ANA_DIZIN, "data")
 SKAB_DIZINI = os.path.join(VERI_DIZINI, "skab")
 BATADAL_DIZINI = os.path.join(VERI_DIZINI, "batadal")
+
+PENCERE_BOYUTU = 4
+ALFABE_BOYUTU = 3
+PCA_BILESEN_SAYISI = 1
+TOHUMLAR = [42, 123, 2026, 7, 999]
+
+BATADAL_BOLME = {
+    "train": 0.60,
+    "val": 0.20,
+    "test": 0.20
+}
