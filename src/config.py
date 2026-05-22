df9fba7888b35faf81ffb6f55d831ebd2561af7b
Yekta Cengiz <yekta.cengiz@kocaeli.edu.tr>
2026-05-22 10:15:00 +0300

    Proje yapisinin kurulmasi ve veri yollari tanimlamalarinin yapilmasi

---
+++ src/config.py
@@ -0,0 +1,6 @@
+import os
+
+ANA_DIZIN = r"D:\yazlab2"
+VERI_DIZINI = os.path.join(ANA_DIZIN, "data")
+SKAB_DIZINI = os.path.join(VERI_DIZINI, "skab")
+BATADAL_DIZINI = os.path.join(VERI_DIZINI, "batadal")
