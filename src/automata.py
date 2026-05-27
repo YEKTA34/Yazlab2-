dc5ba5e2267c3fca6c1a2a24cc74cefe7290d8fd
Yekta Cengiz <yekta.cengiz@kocaeli.edu.tr>
2026-05-27 11:20:00 +0300

    SAX sembolik harf donusumu fonksiyonunun eklenmesi

--- src/automata.py
+++ src/automata.py
@@ -11,3 +11,17 @@ def paa_donusumu(veri, paa_penceresi=1):
         blok = veri[i * paa_penceresi : (i + 1) * paa_penceresi]
         paa_verisi.append(np.mean(blok))
     return np.array(paa_verisi)
+
+def sax_donusumu(paa_verisi, alfabe_boyutu):
+    kirilma_noktalari = stats.norm.ppf(np.linspace(0, 1, alfabe_boyutu + 1)[1:-1])
+    semboller = [chr(97 + i) for i in range(alfabe_boyutu)]
+    sax_dizisi = []
+    for deger in paa_verisi:
+        indeks = 0
+        for kn in kirilma_noktalari:
+            if deger > kn:
+                indeks += 1
+            else:
+                break
+        sax_dizisi.append(semboller[indeks])
+    return "".join(sax_dizisi)
